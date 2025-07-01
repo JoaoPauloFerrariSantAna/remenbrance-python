f2b413ec76a98e36970569a1fa5b76d0368f9305
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2025-07-01 09:51:39 -0300

    ref: removed a, now, unnecessary function
    since we can get_user_id by the user class directly

---
+++ src/userland/queries/user_queries.py
@@ -0,0 +1,13 @@
+from data_handlers.entities import Database
+from timestamps import get_curr_date
+from custom_types import FormatedDate, UserId, ReminderInformation
+
+def get_reminders_details() -> ReminderInformation:
+	"""Will get the reminder details.
+	:return: Returns a list of the reminder details.
+	:rtype: ReminderInformation.
+	"""
+	rdata = get_curr_date()
+	rtitle = input("Reminder's title: ")
+	rdesc = input("Reminder's description: ")
+	return (rdata, rtitle, rdesc)
