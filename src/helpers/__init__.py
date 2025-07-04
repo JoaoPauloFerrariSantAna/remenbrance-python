d55d532f9b076b89048cb130e2fb5b84fa0e4efc
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2025-07-04 14:40:48 -0300

    ref: added helpers to the 'global' projet scope

---
+++ src/helpers/__init__.py
@@ -0,0 +1,1 @@
+from .printers import print_msg, print_err_msg, print_options, print_user_info
---
+++ src/helpers/printers.py
@@ -0,0 +1,40 @@
+from sys import stderr
+from custom_types import FormatedDate
+
+def print_msg(msg: str) -> None:
+	"""Function to show formated message.
+	:param msg: The message to show.
+	:type msg: string.
+	:return: None.
+	"""
+	print()
+	print("----------------------")
+	print(msg)
+	print("----------------------")
+	print()
+
+def print_err_msg(msg: str) -> None:
+	stderr.write("----------------------\n")
+	stderr.write(msg)
+	stderr.write("----------------------\n")
+	stderr.flush()
+
+def print_options() -> None:
+	"""Function to show what the user can do.
+	:return: None.
+	"""
+	print("Would you like to:")
+	print(">>[M]ake a post.")
+	print(">>[C]heck account information.")
+	print(">>[D]elete account.")
+	print(">>[L]ogout")
+
+def print_user_info(name, email, acc_ts: FormatedDate) -> None:
+	"""Will show the fields which user can edit.
+	:param uinfo: User information obtained from get_credentials.
+	:param acc_ts: User account creation date.
+	:type uinfo: UserData.
+	:type acc_ts: FormatedDate.
+	:return: None.
+	"""
+	print_msg(f"\t\t\tACCOUNT INFO:\n- Username: {name}.\n- User email: {email}.\n- Creation date: {acc_ts}.")
