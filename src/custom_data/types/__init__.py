683205bc6ff920180ff093954d9baa3d22c979ca
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2025-07-09 15:40:36 -0300

    feat: forgot the __init__

--- src/custom_data/types/__init__.py
+++ src/custom_data/types/__init__.py
@@ -1,7 +1,11 @@
 from .date_types import FormatedDate
 from .db_types import EnvInfo, InsertData
-from .operation_types import OpStatus
-from .query_types import QueryResult, Query, PreparedQuery, PreparedTypes
+from .query_types import(
+	QueryResult,
+	Query,
+	PreparedQuery,
+	PreparedTypes
+)
 from .user_types import UserId, UserData
 from .reminder_types import ReminderInformation
 from .post_types import PostInfo
